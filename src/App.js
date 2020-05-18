10f13f7c742982b621e35d8341984e9d55550ff4
BShah <thisisbhoomishah@gmail.com>
2020-05-18 13:43:05 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 const particlesOptions = {
@@ -22,10 +25,10 @@ function App() {
 	      params={particlesOptions}
 	    />
       <Navigation />
+      <Logo />
       <Rank />
       <ImageLinkForm />
       {/*  <FaceRecognition /> */}
-      <Logo />
     </div>
   );
 }
